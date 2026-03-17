09a83ba57edbca3ee606404f4e0c4df193e8608d
Daniela Bordeianu <danabordeianu55@gmail.com>
2026-03-16 20:38:14 -0400

    chore: initial project structure

---
+++ src/main/java/com/group28/util/DBConnection.java
@@ -0,0 +1,30 @@
+package com.group28.util;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+    private static Connection connection=null;
+    public static Connection getConnection() throws SQLException {
+        if (connection == null) {
+            try {
+                Properties props = new Properties();
+                InputStream input = DBConnection.class.getClassLoader()
+                        .getResourceAsStream("db.properties");
+                props.load(input);
+                String url = props.getProperty("db.url");
+                String user = props.getProperty("db.username");
+                String password = props.getProperty("db.password");
+
+                Class.forName("org.postgresql.Driver");
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (Exception e) {
+                throw new SQLException("Failed to load database properties: " + e.getMessage());
+            }
+        }
+        return connection;
+    }
+}
