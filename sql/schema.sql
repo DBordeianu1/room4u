a84032362afe2065e8fb0cfe73a4318508b56c33
Daniela Bordeianu <danabordeianu55@gmail.com>
2026-03-21 16:41:07 -0400

    fix: correct some of the ddl statements

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,20 +1,25 @@
 SET search_path = 'public';
+DROP TABLE IF EXISTS processes, supervises, works_at, makes, reg_room, 
+customer, employee_role, employee, person, renting, booking, 
+registration_special_request, registration, room_amenity, room_problem, 
+room_view, room, hotel_phone, hotel_email, hotel, hotel_chain_phone, 
+hotel_chain_email, hotel_chain;
 -- ddl statements
 -- hotel chain related tables
+-- name was changed to chain_name because recognized as sql keyword
 CREATE TABLE hotel_chain(
-	chain_id numeric(1,0) CHECK(chain_id BETWEEN 1 AND 5),
-	name varchar(100) NOT NULL,
+	chain_id numeric(1,0) CHECK(chain_id BETWEEN 1 AND 5) PRIMARY KEY,
+	chain_name varchar(100) NOT NULL,
 	street_number varchar(10) NOT NULL,
 	city varchar(20) NOT NULL,
 	state_province varchar(50) NOT NULL,
 	zip_postal_code varchar(10) NOT NULL,
-	country varchar(10) NOT NULL CHECK(country='canada' OR country='us'),
-	PRIMARY KEY(chain_id)
+	country varchar(10) NOT NULL CHECK(country='canada' OR country='us')
 );
 
 CREATE TABLE hotel_chain_email(
 	chain_id numeric(1,0) CHECK(chain_id BETWEEN 1 AND 5),
-	email_address varchar(100) CHECK (email LIKE '_%@_%._%'),
+	email_address varchar(100) CHECK (email_address LIKE '_%@_%._%'),
 	email_type varchar(100),
 	PRIMARY KEY(chain_id,email_address,email_type),
 	FOREIGN KEY(chain_id) REFERENCES hotel_chain
@@ -23,17 +28,18 @@ CREATE TABLE hotel_chain_email(
 CREATE TABLE hotel_chain_phone(
 	chain_id numeric(1,0) CHECK(chain_id BETWEEN 1 AND 5),
 	phone_number varchar(25),
-	phone_type varchar(100)
+	phone_type varchar(100),
 	PRIMARY KEY(chain_id,phone_number,phone_type),
 	FOREIGN KEY(chain_id) REFERENCES hotel_chain
 );
 
 -- hotel related tables
+-- name was changed to hotel_name because recognized as sql keyword
 CREATE TABLE hotel(
 	hotel_id bigserial PRIMARY KEY,
-	chain_id numeric(1,0) CHECK(chain_id BETWEEN 1 AND 5),
-	name varchar(100) NOT NULL,
-	classification numeric(1,0) CHECK classification BETWEEN 1 AND 5),
+	chain_id numeric(1,0) NOT NULL CHECK(chain_id BETWEEN 1 AND 5),
+	hotel_name varchar(100) NOT NULL,
+	classification numeric(1,0) NOT NULL CHECK(classification BETWEEN 1 AND 5),
 	street_number varchar(10) NOT NULL,
 	city varchar(20) NOT NULL,
 	state_province varchar(50) NOT NULL,
@@ -43,15 +49,15 @@ CREATE TABLE hotel(
 );
 
 CREATE TABLE hotel_email(
-	hotel_id bigserial,
-	email_address varchar(100) CHECK (email LIKE '_%@_%._%'),
+	hotel_id bigint,
+	email_address varchar(100) CHECK (email_address LIKE '_%@_%._%'),
 	email_type varchar(100),
 	PRIMARY KEY(hotel_id,email_address,email_type),
 	FOREIGN KEY(hotel_id) REFERENCES hotel
 );
 
 CREATE TABLE hotel_phone(
-	hotel_id bigserial,
+	hotel_id bigint,
 	phone_number varchar(25),
 	phone_type varchar(100),
 	PRIMARY KEY(hotel_id,phone_number,phone_type),
@@ -64,7 +70,7 @@ CREATE TABLE hotel_phone(
 -- this change was required to minimize confusion
 -- same thing applies for view which becomes view_of_room
 CREATE TABLE room(
-	hotel_id bigserial,
+	hotel_id bigint,
 	room_number integer CHECK(room_number BETWEEN 1 AND 1000),
 	price numeric(6,2) NOT NULL,
 	capacity varchar(10) NOT NULL CHECK(capacity='single' OR capacity='double' OR capacity='suite' OR capacity='family'),
@@ -74,30 +80,27 @@ CREATE TABLE room(
 );
 
 CREATE TABLE room_view(
-	hotel_id bigserial,
+	hotel_id bigint,
 	room_number integer CHECK(room_number BETWEEN 1 AND 1000),
 	view_of_room varchar(10) CHECK(view_of_room='mountain' OR view_of_room='sea' OR view_of_room='none'),
-	PRIMARY KEY(hotel_id,room_number,view_of_room)
-	FOREIGN KEY(hotel_id) REFERENCES hotel,
-	FOREIGN KEY(room_number) REFERENCES room
+	PRIMARY KEY(hotel_id,room_number,view_of_room),
+	FOREIGN KEY(hotel_id,room_number) REFERENCES room(hotel_id,room_number)
 );
 
 CREATE TABLE room_problem(
-	hotel_id bigserial,
+	hotel_id bigint,
 	room_number integer CHECK(room_number BETWEEN 1 AND 1000),
 	problem varchar(100),
 	PRIMARY KEY(hotel_id,room_number,problem),
-	FOREIGN KEY(hotel_id) REFERENCES hotel,
-	FOREIGN KEY(room_number) REFERENCES room
+	FOREIGN KEY(hotel_id,room_number) REFERENCES room(hotel_id,room_number)
 );
 
 CREATE TABLE room_amenity(
-	hotel_id bigserial,
+	hotel_id bigint,
 	room_number integer CHECK(room_number BETWEEN 1 AND 1000),
 	amenity varchar(100),
 	PRIMARY KEY(hotel_id,room_number,amenity),
-	FOREIGN KEY(hotel_id) REFERENCES hotel,
-	FOREIGN KEY(room_number) REFERENCES room	
+	FOREIGN KEY(hotel_id,room_number) REFERENCES room(hotel_id,room_number)	
 );
 
 -- registration related tables
@@ -105,25 +108,26 @@ CREATE TABLE registration(
 	registration_id bigserial PRIMARY KEY,
 	start_date timestamp NOT NULL,
 	end_date timestamp NOT NULL,
-	is_archived boolean NOT NULL
+	is_archived boolean NOT NULL,
+	CHECK(start_date<end_date)
 );
 
 CREATE TABLE registration_special_request(
-	registration_id bigserial,
+	registration_id bigint,
 	special_request varchar(100),
 	PRIMARY KEY(registration_id,special_request),
 	FOREIGN KEY(registration_id) REFERENCES registration
 );
 
 CREATE TABLE booking(
-	registration_id bigserial PRIMARY KEY,
-	status varchar(20) NOT NULL,
+	registration_id bigint PRIMARY KEY,
+	status varchar(20) NOT NULL CHECK(status='pending' OR status='cancelled' OR status='confirmed'),
 	booking_date timestamp NOT NULL,
 	FOREIGN KEY(registration_id) REFERENCES registration
 );
 
 CREATE TABLE renting(
-	registration_id bigserial PRIMARY KEY,
+	registration_id bigint PRIMARY KEY,
 	paid boolean NOT NULL,
 	renting_date timestamp NOT NULL,
 	FOREIGN KEY(registration_id) REFERENCES registration
@@ -131,7 +135,7 @@ CREATE TABLE renting(
 
 --person related tables
 CREATE TABLE person(
-	id_number bigserial,
+	id_number numeric(9,0) CHECK(id_number>0),
 	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
 	first_name varchar(100) NOT NULL,
 	middle_name varchar(100),
@@ -140,30 +144,32 @@ CREATE TABLE person(
 	city varchar(20) NOT NULL,
 	state_province varchar(50) NOT NULL,
 	zip_postal_code varchar(10) NOT NULL,
-	country varchar(10) NOT NULL CHECK(country='canada' OR country='us')
+	country varchar(10) NOT NULL CHECK(country='canada' OR country='us'),
 	PRIMARY KEY(id_number,id_type)
 );
 
 CREATE TABLE employee(
-	id_number bigserial,
+	id_number bigint,
 	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
-	hotel_id bigserial PRIMARY KEY,
-	PRIMARY KEY(id_number,id_type,hotel_id),
-	FOREIGN KEY(id_number,id_type) REFERENCES person
+	hotel_id bigint,
+	PRIMARY KEY(id_number,id_type),
+	FOREIGN KEY(id_number,id_type) REFERENCES person,
+	FOREIGN KEY(hotel_id) REFERENCES hotel
 );
 
+-- role was changed to employee_role as it is perceived as a keyword in sql
 CREATE TABLE employee_role(
-	id_number bigserial,
+	id_number bigint,
 	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
-	role varchar(100),
-	PRIMARY KEY(id_number,id_type,role),
-	FOREIGN KEY(id_number,id_type) REFERENCES person
+	employee_role varchar(100),
+	PRIMARY KEY(id_number,id_type,employee_role),
+	FOREIGN KEY(id_number,id_type) REFERENCES employee
 );
 
 CREATE TABLE customer(
-	id_number bigserial,
+	id_number bigint,
 	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
-	date_of_registration timestamp,
+	date_of_registration timestamp NOT NULL,
 	PRIMARY KEY(id_number,id_type),
 	FOREIGN KEY(id_number,id_type) REFERENCES person
 );
@@ -171,19 +177,18 @@ CREATE TABLE customer(
 -- "relationships"
 -- registration and room
 CREATE TABLE reg_room(
-	registration_id bigserial PRIMARY KEY,
-	hotel_id bigserial,
+	registration_id bigint PRIMARY KEY,
+	hotel_id bigint,
 	room_number integer CHECK(room_number BETWEEN 1 AND 1000),
 	FOREIGN KEY(registration_id) REFERENCES registration,
-	FOREIGN KEY(hotel_id) REFERENCES hotel,
-	FOREIGN KEY(room_number) REFERENCES room
+	FOREIGN KEY(hotel_id,room_number) REFERENCES room(hotel_id,room_number)
 );
 
 -- registration and customer
 -- unlike the current schema diagram, the attributes for customer are not preceded by customer
 CREATE TABLE makes(
-	registration_id bigserial PRIMARY KEY,
-	id_number bigserial,
+	registration_id bigint PRIMARY KEY,
+	id_number bigint,
 	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
 	FOREIGN KEY(registration_id) REFERENCES registration,
 	FOREIGN KEY(id_number,id_type) REFERENCES customer
@@ -191,9 +196,9 @@ CREATE TABLE makes(
 
 -- employee and hotel
 CREATE TABLE works_at(
-	id_number bigserial,
+	id_number bigint,
 	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
-	hotel_id bigserial,
+	hotel_id bigint,
 	PRIMARY KEY(id_number,id_type),
 	FOREIGN KEY(id_number,id_type) REFERENCES employee,
 	FOREIGN KEY(hotel_id) REFERENCES hotel
@@ -201,20 +206,21 @@ CREATE TABLE works_at(
 
 -- employee with itself
 CREATE TABLE supervises(
-	employee_id_number bigserial,
-	employee_id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
-	manager_id_number bigserial,
-	manager_id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
-	PRIMARY KEY(employee_id_number,employee_id_type,manager_id_number,manager_id_type),
-	FOREIGN KEY(employee_id_number,employee_id_type,manager_id_number,manager_id_type) REFERENCES employee
+	employee_id_number bigint,
+	employee_id_type varchar(3) CHECK (employee_id_type='sin' OR employee_id_type='ssn'),
+	manager_id_number bigint,
+	manager_id_type varchar(3) CHECK (manager_id_type='sin' OR manager_id_type='ssn'),
+	PRIMARY KEY(employee_id_number,employee_id_type),
+	FOREIGN KEY(employee_id_number,employee_id_type) REFERENCES employee(id_number,id_type),
+	FOREIGN KEY(manager_id_number,manager_id_type) REFERENCES employee(id_number,id_type)
 );
 
 -- employee and renting
 CREATE TABLE processes(
-	id_number bigserial,
+	id_number bigint,
 	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
-	registration_id bigserial,
-	PRIMARY KEY(id_number,id_type)
-	FOREIGN KEY(id_number,id_type) REFERENCES employee
+	registration_id bigint,
+	PRIMARY KEY(id_number,id_type),
+	FOREIGN KEY(id_number,id_type) REFERENCES employee,
 	FOREIGN KEY(registration_id) REFERENCES registration
 );
