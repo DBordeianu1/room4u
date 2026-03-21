22459a4e0a81c1e0be4bf557bf032af202b3967f
Daniela Bordeianu <danabordeianu55@gmail.com>
2026-03-21 15:33:18 -0400

    draft: add final ddl statements

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE hotel_chain(
 	city varchar(20) NOT NULL,
 	state_province varchar(50) NOT NULL,
 	zip_postal_code varchar(10) NOT NULL,
-	country varchar(10) NOT NULL CHECK(country='Canada' OR country='US'),
+	country varchar(10) NOT NULL CHECK(country='canada' OR country='us'),
 	PRIMARY KEY(chain_id)
 );
 
@@ -38,7 +38,7 @@ CREATE TABLE hotel(
 	city varchar(20) NOT NULL,
 	state_province varchar(50) NOT NULL,
 	zip_postal_code varchar(10) NOT NULL,
-	country varchar(10) NOT NULL CHECK(country='Canada' OR country='US'),
+	country varchar(10) NOT NULL CHECK(country='canada' OR country='us'),
 	FOREIGN KEY(chain_id) REFERENCES hotel_chain
 );
 
@@ -62,7 +62,7 @@ CREATE TABLE hotel_phone(
 
 -- although room_number is called number in the relational schema diagram,
 -- this change was required to minimize confusion
--- same thing applies for view which becomes view of room
+-- same thing applies for view which becomes view_of_room
 CREATE TABLE room(
 	hotel_id bigserial,
 	room_number integer CHECK(room_number BETWEEN 1 AND 1000),
@@ -129,8 +129,44 @@ CREATE TABLE renting(
 	FOREIGN KEY(registration_id) REFERENCES registration
 );
 
--- employee related tables
--- customer related tables
+--person related tables
+CREATE TABLE person(
+	id_number bigserial,
+	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
+	first_name varchar(100) NOT NULL,
+	middle_name varchar(100),
+	last_name varchar(100) NOT NULL,
+	street_number varchar(10) NOT NULL,
+	city varchar(20) NOT NULL,
+	state_province varchar(50) NOT NULL,
+	zip_postal_code varchar(10) NOT NULL,
+	country varchar(10) NOT NULL CHECK(country='canada' OR country='us')
+	PRIMARY KEY(id_number,id_type)
+);
+
+CREATE TABLE employee(
+	id_number bigserial,
+	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
+	hotel_id bigserial PRIMARY KEY,
+	PRIMARY KEY(id_number,id_type,hotel_id),
+	FOREIGN KEY(id_number,id_type) REFERENCES person
+);
+
+CREATE TABLE employee_role(
+	id_number bigserial,
+	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
+	role varchar(100),
+	PRIMARY KEY(id_number,id_type,role),
+	FOREIGN KEY(id_number,id_type) REFERENCES person
+);
+
+CREATE TABLE customer(
+	id_number bigserial,
+	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
+	date_of_registration timestamp,
+	PRIMARY KEY(id_number,id_type),
+	FOREIGN KEY(id_number,id_type) REFERENCES person
+);
 
 -- "relationships"
 -- registration and room
@@ -144,4 +180,41 @@ CREATE TABLE reg_room(
 );
 
 -- registration and customer
-CREATE TABLE makes();
+-- unlike the current schema diagram, the attributes for customer are not preceded by customer
+CREATE TABLE makes(
+	registration_id bigserial PRIMARY KEY,
+	id_number bigserial,
+	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
+	FOREIGN KEY(registration_id) REFERENCES registration,
+	FOREIGN KEY(id_number,id_type) REFERENCES customer
+);
+
+-- employee and hotel
+CREATE TABLE works_at(
+	id_number bigserial,
+	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
+	hotel_id bigserial,
+	PRIMARY KEY(id_number,id_type),
+	FOREIGN KEY(id_number,id_type) REFERENCES employee,
+	FOREIGN KEY(hotel_id) REFERENCES hotel
+);
+
+-- employee with itself
+CREATE TABLE supervises(
+	employee_id_number bigserial,
+	employee_id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
+	manager_id_number bigserial,
+	manager_id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
+	PRIMARY KEY(employee_id_number,employee_id_type,manager_id_number,manager_id_type),
+	FOREIGN KEY(employee_id_number,employee_id_type,manager_id_number,manager_id_type) REFERENCES employee
+);
+
+-- employee and renting
+CREATE TABLE processes(
+	id_number bigserial,
+	id_type varchar(3) CHECK (id_type='sin' OR id_type='ssn'),
+	registration_id bigserial,
+	PRIMARY KEY(id_number,id_type)
+	FOREIGN KEY(id_number,id_type) REFERENCES employee
+	FOREIGN KEY(registration_id) REFERENCES registration
+);
